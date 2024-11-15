2b1e310f9e7d0c62b7397bdeb0bd3de3fd89cf88
Poole <132571904+Poole507@users.noreply.github.com>
2024-11-15 10:57:52 +0800

    删除没有必要的

--- src/main/java/com/hsasys/controller/UserController.java
+++ src/main/java/com/hsasys/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.hsasys.controller;
 
-import com.hsasys.DecisionTree;
 import com.hsasys.controller.tools.Code;
 import com.hsasys.controller.tools.Result;
 import com.hsasys.domain.User;
-import com.hsasys.domain.UserType;
 import com.hsasys.service.insert.IUserService;
 import com.hsasys.service.select.SUserService;
 import com.hsasys.service.update.UUserService;
@@ -81,86 +79,84 @@ public class UserController {
         return uUserService.updateUserInfo(user);
     }
 
-    @RequestMapping("/recommend")
-    Result recommendFood(@RequestParam("userId") Integer userId){
-        Double BMI = sUserService.getBMI( userId );
-        ArrayList<String> featLabels = new ArrayList<>();
-        if(BMI<18.5){
-            ArrayList<Object[]> dataSet = new ArrayList<>();
-            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{0, 0, 0, 1, "no"});
-            dataSet.add(new Object[]{0, 1, 0, 1, "yes"});
-            dataSet.add(new Object[]{0, 1, 1, 0, "yes"});
-            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{1, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{1, 0, 0, 1, "no"});
-            dataSet.add(new Object[]{1, 1, 1, 1, "yes"});
-            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 1, 1, "yes"});
-            dataSet.add(new Object[]{2, 1, 0, 1, "yes"});
-            dataSet.add(new Object[]{2, 1, 0, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 0, 0, "no"});
-            ArrayList<String> labels = new ArrayList<>();
-            labels.add("水果");
-            labels.add("甜品");
-            labels.add("坚果，鱼类，蛋白");
-            labels.add("零食");
-            labels.add("偏瘦");
-            DecisionTree.TreeNode tree = DecisionTree.createTree( dataSet, labels, featLabels );
-            return new Result(Code.OK,tree.label);
-        }else if(BMI>=18.5&&BMI<25){
-            ArrayList<Object[]> dataSet = new ArrayList<>();
-            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{0, 0, 0, 1, "no"});
-            dataSet.add(new Object[]{0, 1, 0, 1, "yes"});
-            dataSet.add(new Object[]{0, 1, 1, 0, "yes"});
-            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{1, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{1, 0, 0, 1, "no"});
-            dataSet.add(new Object[]{1, 1, 1, 1, "yes"});
-            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 1, 1, "yes"});
-            dataSet.add(new Object[]{2, 1, 0, 1, "yes"});
-            dataSet.add(new Object[]{2, 1, 0, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 0, 0, "no"});
-            ArrayList<String> labels = new ArrayList<>();
-            labels.add("水果");
-            labels.add("甜品");
-            labels.add("燕麦，瘦肉");
-            labels.add("零食");
-            labels.add("正常");
-            DecisionTree.TreeNode tree = DecisionTree.createTree( dataSet, labels, featLabels );
-            return new Result(Code.OK,tree.label);
-        }else{
-            ArrayList<Object[]> dataSet = new ArrayList<>();
-            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{0, 0, 0, 1, "no"});
-            dataSet.add(new Object[]{0, 1, 0, 1, "yes"});
-            dataSet.add(new Object[]{0, 1, 1, 0, "yes"});
-            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{1, 0, 0, 0, "no"});
-            dataSet.add(new Object[]{1, 0, 0, 1, "no"});
-            dataSet.add(new Object[]{1, 1, 1, 1, "yes"});
-            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 1, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 1, 1, "yes"});
-            dataSet.add(new Object[]{2, 1, 0, 1, "yes"});
-            dataSet.add(new Object[]{2, 1, 0, 2, "yes"});
-            dataSet.add(new Object[]{2, 0, 0, 0, "no"});
-            ArrayList<String> labels = new ArrayList<>();
-            labels.add("水果");
-            labels.add("甜品");
-            labels.add("蔬菜水果");
-            labels.add("零食");
-            labels.add("偏胖");
-            DecisionTree.TreeNode tree = DecisionTree.createTree( dataSet, labels, featLabels );
-            return new Result(Code.OK,tree.label);
-        }
-
-    }
+//    @RequestMapping("/recommend")
+//    Result recommendFood(@RequestParam("userId") Integer userId){
+//        Double BMI = sUserService.getBMI( userId );
+//        ArrayList<String> featLabels = new ArrayList<>();
+//        if(BMI<18.5){
+//            ArrayList<Object[]> dataSet = new ArrayList<>();
+//            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{0, 0, 0, 1, "no"});
+//            dataSet.add(new Object[]{0, 1, 0, 1, "yes"});
+//            dataSet.add(new Object[]{0, 1, 1, 0, "yes"});
+//            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{1, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{1, 0, 0, 1, "no"});
+//            dataSet.add(new Object[]{1, 1, 1, 1, "yes"});
+//            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 1, 1, "yes"});
+//            dataSet.add(new Object[]{2, 1, 0, 1, "yes"});
+//            dataSet.add(new Object[]{2, 1, 0, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 0, 0, "no"});
+//            ArrayList<String> labels = new ArrayList<>();
+//            labels.add("水果");
+//            labels.add("甜品");
+//            labels.add("坚果，鱼类，蛋白");
+//            labels.add("零食");
+//            labels.add("偏瘦");
+//            DecisionTree.TreeNode tree = DecisionTree.createTree( dataSet, labels, featLabels );
+//            return new Result(Code.OK,tree.label);
+//        }else if(BMI>=18.5&&BMI<25){
+//            ArrayList<Object[]> dataSet = new ArrayList<>();
+//            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{0, 0, 0, 1, "no"});
+//            dataSet.add(new Object[]{0, 1, 0, 1, "yes"});
+//            dataSet.add(new Object[]{0, 1, 1, 0, "yes"});
+//            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{1, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{1, 0, 0, 1, "no"});
+//            dataSet.add(new Object[]{1, 1, 1, 1, "yes"});
+//            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 1, 1, "yes"});
+//            dataSet.add(new Object[]{2, 1, 0, 1, "yes"});
+//            dataSet.add(new Object[]{2, 1, 0, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 0, 0, "no"});
+//            ArrayList<String> labels = new ArrayList<>();
+//            labels.add("水果");
+//            labels.add("甜品");
+//            labels.add("燕麦，瘦肉");
+//            labels.add("零食");
+//            labels.add("正常");
+//            DecisionTree.TreeNode tree = DecisionTree.createTree( dataSet, labels, featLabels );
+//            return new Result(Code.OK,tree.label);
+//        }else{
+//            ArrayList<Object[]> dataSet = new ArrayList<>();
+//            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{0, 0, 0, 1, "no"});
+//            dataSet.add(new Object[]{0, 1, 0, 1, "yes"});
+//            dataSet.add(new Object[]{0, 1, 1, 0, "yes"});
+//            dataSet.add(new Object[]{0, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{1, 0, 0, 0, "no"});
+//            dataSet.add(new Object[]{1, 0, 0, 1, "no"});
+//            dataSet.add(new Object[]{1, 1, 1, 1, "yes"});
+//            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{1, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 1, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 1, 1, "yes"});
+//            dataSet.add(new Object[]{2, 1, 0, 1, "yes"});
+//            dataSet.add(new Object[]{2, 1, 0, 2, "yes"});
+//            dataSet.add(new Object[]{2, 0, 0, 0, "no"});
+//            ArrayList<String> labels = new ArrayList<>();
+//            labels.add("水果");
+//            labels.add("甜品");
+//            labels.add("蔬菜水果");
+//            labels.add("零食");
+//            labels.add("偏胖");
+//            DecisionTree.TreeNode tree = DecisionTree.createTree( dataSet, labels, featLabels );
+//            return new Result(Code.OK,tree.label);
+//        }
 }
